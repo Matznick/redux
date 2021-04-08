0b100b61d73483954da9b7fd7b1da44b982d3e0b
matznick <matznick@Fakulta314siMac.fritz.box>
2021-04-08 10:41:28 +0200

    checkpoint submitted

--- src/components/nav.test.js
+++ src/components/nav.test.js
@@ -1,24 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Nav from "./Nav";
-import configureMockStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-
-const middlewares = [thunk];
-const mockstore = configureMockStore(middlewares);
 
 describe("Nav test", () => {
-  it("click sendToRedux button", () => {
-    const store = mockstore({});
-
-    const wrapper = shallow(
-      <Provider store={store}>
-        <Nav />
-      </Provider>
-    );
-    const wrap = wrapper.find("Connect(Nav)");
-    console.log("wrap is : ", wrap);
-    expect(wrap).toHaveLength(1);
+  it("render Nav correctly", () => {
+    const wrapper = shallow(<Nav.WrappedComponent />);
+    expect(wrapper.find(".nav")).toHaveLength(1);
   });
 });
